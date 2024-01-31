71c981d1382eea0551f15fadf8ad1cd165c9075d
JahanzaibAns <jahanzebansari503@gmail.com>
2024-01-31 06:08:40 +0500

    lp completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,3 +16,51 @@ $(document).ready(function () {
 		}
 	});
 });
+
+
+
+
+// Place your existing initCarousel function here
+$(document).ready(function () {
+	$('.js-carousel').slick({
+	  infinite: true,
+	  slidesToShow: 2,
+	  slidesToScroll: 1,
+	  appendArrows: '.js-sliderArrows',
+	  appendDots: '.js-sliderDots',
+	  prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button"><i class="YOUR_ARROW_LEFT_CLASS"></i></button>',
+	  nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"><i class="YOUR_ARROW_RIGHT_CLASS"></i></button>',
+	  centerMode: false,
+	  centerPadding: '60px',
+	  dots: true,
+	  autoplay: true,
+	  autoplaySpeed: 4000,
+	  responsive: [
+		{
+		  breakpoint: 1600,
+		  settings: {
+			slidesToShow: 2
+		  }
+		},
+		{
+		  breakpoint: 992,
+		  settings: {
+			slidesToShow: 2
+		  }
+		},
+		{
+		  breakpoint: 768,
+		  settings: {
+			slidesToShow: 1
+		  }
+		},
+		{
+		  breakpoint: 375,
+		  settings: {
+			slidesToShow: 1
+		  }
+		}
+	  ]
+	});
+  });
+
