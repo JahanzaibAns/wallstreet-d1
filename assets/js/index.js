a7b4a995403aa1e99f8f4f4d658f44c73b4d35ac
JahanzaibAns <jahanzebansari503@gmail.com>
2024-02-05 05:22:49 +0500

    new project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,73 +1,27 @@
-$(document).ready(function () {
-	$('.js-carousel').slick({
-		infinite: true,
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		appendArrows: '.js-sliderArrows',
-		appendDots: '.js-sliderDots',
-		prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button"><i class="YOUR_ARROW_LEFT_CLASS"></i></button>',
-		nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"><i class="YOUR_ARROW_RIGHT_CLASS"></i></button>',
-		centerMode: false,
-		centerPadding: '60px',
-		dots: true,
-		autoplay: true,
-		autoplaySpeed: 4000,
-		responsive: [
-			{
-				breakpoint: 1600,
-				settings: {
-					slidesToShow: 2
-				}
-			},
-			{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 2
-				}
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1
-				}
-			},
-			{
-				breakpoint: 375,
-				settings: {
-					slidesToShow: 1
-				}
-			}
-		]
-	});
-});
-
-document.getElementById('phoneNo').addEventListener('input', function (e) {
-	var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-	e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-});
-
-
-// Function to clear local storage
-function clearLocalStorage() {
-	localStorage.removeItem('registrationData');
+function openNav() {
+	document.getElementById("sidebar").style.width = "250px";
 }
 
-// Function to save data to local storage and redirect to the next page
-const saveDataAndRedirect = () => {
-	// Retrieve form data
-	const firstName = document.getElementById('firstName').value;
-	// const lastName = document.getElementById('lastName').value;
-	const email = document.getElementById('email').value;
-	const phoneNo = document.getElementById('phoneNo').value;
+function closeNav() {
+	document.getElementById("sidebar").style.width = "0";
+}
 
-	// Store data in local storage
-	const dataToStore = { firstName, email, phoneNo };
-	localStorage.setItem('registrationData', JSON.stringify(dataToStore));
+ // Get the navigation bar element
+ var navbar = document.getElementById("header-sticky");
 
-	// Log the stored data
-	console.log('Stored data:', dataToStore);
+ // Get the offset position of the navbar
+ var sticky = navbar.offsetTop;
 
-	// Redirect to the next page
-	window.location.href = 'four-steps-form.html';
-};
+ // Function to add or remove the "sticky" class based on scroll position
+ function stickyNavbar() {
+	 if (window.pageYOffset >= sticky) {
+		 navbar.classList.add("sticky");
+	 } else {
+		 navbar.classList.remove("sticky");
+	 }
+ }
 
+ // Attach the function to the scroll event
+ window.onscroll = function () {
+	 stickyNavbar();
+ };
